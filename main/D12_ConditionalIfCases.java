8f911d1c205083630d1fd8810c38679b659d2170
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-02-07 09:17:29 +0100

    Add Demo 12 and Demo W1

---
+++ main/D12_ConditionalIfCases.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * Bedingte Zuweisungen
+ * 
+ * @author Daniel Appenmaier
+ * @version 1.0
+ */
+public class D12_ConditionalIfCases{
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String ageText;
+        
+        System.out.print("Gib bitte Dein Alter ein: ");
+        int age = scanner.nextInt();
+        
+        if (age >= 18) {
+            ageText = "volljaehrig";
+        } else {
+            ageText = "minderjaehrig";
+        }
+        
+        ageText = (age >= 18) ? "volljaehrig" : "minderjaehrig";
+        
+        System.out.println("Du bist " + ageText); 
+        
+    }
+    
+}
---
+++ main/W01.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ * Wiederholung
+ * 
+ * @author Daniel Appenmaier
+ * @version 1.0
+ */
+public class W01 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Was moechtest Du tun?");
+        System.out.println("1 - Sinus berechnen");
+        System.out.println("2 - Zufallszahl erzeugen");
+        System.out.println("3 - Ausdruck auswerten");
+        
+        int answer = scanner.nextInt();
+        
+        if (answer == 1) {
+            System.out.print("Bitte gib eine reelle Zahl ein: ");
+            double value = scanner.nextDouble();
+            double result = Math.sin(value);
+            System.out.printf("Der Sinus von %.2f ist %.2f", value, result);
+        } else if (answer == 2) {
+            Random random = new Random();
+            int randomNumber = random.nextInt(1, 11);
+            System.out.println(randomNumber);
+        } else {
+            System.out.print("Bitte gib eine Zahl groesser Null ein: ");
+            int value = scanner.nextInt();
+            if (value > 0) {
+                System.out.println("Super gemacht");
+            } else {
+                System.out.println("Schade, knapp daneben");
+            }
+        }    
+
+    }
+    
+}
