cf7405d80f057f5a6940109751eeb0fdb1cd226b
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2024-08-09 15:57:20 +0200

    Update ShopTest

--- test/ShopTest.java
+++ test/ShopTest.java
@@ -7,8 +7,6 @@ import java.util.HashMap;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import model.FastFood;
-import model.FastFoodCategory;
 import model.InvalidRatingException;
 import model.NoProductFoundException;
 import model.Shop;
@@ -22,36 +20,46 @@ import model.Shop;
  */
 public class ShopTest {
 
-  Shop<FastFood> shop;
-  FastFood ff1;
-  FastFood ff2;
-  FastFood ff3;
+  private static record ProductStub(int returnValue) implements Comparable<ProductStub> {
+
+    @Override
+    public int compareTo(ProductStub o) {
+      return returnValue;
+    }
+
+  }
+
+  Shop<ProductStub> shop;
+  ProductStub product1;
+  ProductStub product2;
+
+  ProductStub product3;
 
   @BeforeEach
   void setUp() {
-    shop = new Shop<FastFood>("MyShop", new HashMap<>());
-    ff1 = new FastFood("FF1", FastFoodCategory.BURGER, 5, false);
-    ff2 = new FastFood("FF2", FastFoodCategory.BURGER, 3, false);
-    ff3 = new FastFood("FF3", FastFoodCategory.BURGER, 1, false);
+    shop = new Shop<>("MyShop", new HashMap<>());
+    product1 = new ProductStub(-5);
+    product2 = new ProductStub(0);
+    product3 = new ProductStub(5);
   }
 
   @Test
   void testAddProduct() {
-    shop.addProduct(ff1);
+    shop.addProduct(product1);
     assertNotNull(shop.assortment()
-        .get(ff1));
+        .get(product1));
   }
 
   @Test
   void testGetAllProductsSortedByNaturalOrdering() {
-    shop.addProduct(ff2);
-    shop.addProduct(ff1);
-    shop.addProduct(ff3);
-    assertEquals(ff1, shop.getAllProductsSortedByNaturalOrdering()
+    shop.addProduct(product2);
+    shop.addProduct(product1);
+    shop.addProduct(product3);
+    assertEquals(product1, shop.getAllProductsSortedByNaturalOrdering()
         .get(0));
-    assertEquals(ff2, shop.getAllProductsSortedByNaturalOrdering()
+    assertEquals(product2, shop.getAllProductsSortedByNaturalOrdering()
         .get(1));
-    assertEquals(ff3, shop.getAllProductsSortedByNaturalOrdering()
+    assertEquals(product3, shop.getAllProductsSortedByNaturalOrdering()
         .get(2));
   }
 
@@ -59,31 +67,31 @@ public class ShopTest {
   void testGetBestRatedProduct() throws NoProductFoundException, InvalidRatingException {
     assertEquals(Optional.empty(), shop.getBestRatedProduct());
 
-    shop.addProduct(ff1);
-    shop.addProduct(ff2);
-    shop.rateProduct(ff1, 5);
-    shop.rateProduct(ff2, 3);
-    assertEquals(Optional.of(ff1), shop.getBestRatedProduct());
-    assertEquals(ff1, shop.getBestRatedProduct()
+    shop.addProduct(product1);
+    shop.addProduct(product2);
+    shop.rateProduct(product1, 5);
+    shop.rateProduct(product2, 3);
+    assertEquals(Optional.of(product1), shop.getBestRatedProduct());
+    assertEquals(product1, shop.getBestRatedProduct()
         .get());
   }
 
   @Test
   void testRateProduct() throws NoProductFoundException, InvalidRatingException {
-    assertThrows(NoProductFoundException.class, () -> shop.rateProduct(ff1, 1));
+    assertThrows(NoProductFoundException.class, () -> shop.rateProduct(product1, 1));
 
-    shop.addProduct(ff1);
-    assertThrows(InvalidRatingException.class, () -> shop.rateProduct(ff1, 0));
-    assertThrows(InvalidRatingException.class, () -> shop.rateProduct(ff1, 6));
+    shop.addProduct(product1);
+    assertThrows(InvalidRatingException.class, () -> shop.rateProduct(product1, 0));
+    assertThrows(InvalidRatingException.class, () -> shop.rateProduct(product1, 6));
 
-    shop.rateProduct(ff1, 3);
-    shop.rateProduct(ff1, 1);
-    shop.rateProduct(ff1, 5);
+    shop.rateProduct(product1, 3);
+    shop.rateProduct(product1, 1);
+    shop.rateProduct(product1, 5);
     assertEquals(3, shop.assortment()
-        .get(ff1)
+        .get(product1)
         .size());
     assertEquals(1, shop.assortment()
-        .get(ff1)
+        .get(product1)
         .get(1));
   }
 
